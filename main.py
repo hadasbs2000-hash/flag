8635e02fe9224b832495f852ca7f5ecc84668303
hadasbs2000-hash <hadasbs2000@gmail.com>
2026-09-08 10:59:15 +0300

    consts added

---
+++ main.py
@@ -0,0 +1,18 @@
+import consts
+import pygame
+
+state = {
+    "solider_index":[0,0],
+    "is_window_open": True,
+    "state": ""
+}
+
+def main():
+    pygame.init()
+    game_fild.create()
+
+    while state["is_window_open"]:
+
+        handle_user_events()
+        if state[]:
+    pass
